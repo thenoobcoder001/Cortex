70242174a59e261ee0d309cc6cec643a0f1f1269
Chirag <sonubag1301@gmail.com>
2026-04-15 20:23:32 +0530

    release version fixes

--- desktop_app/web/vite.config.js
+++ desktop_app/web/vite.config.js
@@ -4,6 +4,7 @@ const react = require("@vitejs/plugin-react");
 
 module.exports = defineConfig({
   root: __dirname,
+  base: "./",
   plugins: [react()],
   build: {
     outDir: path.resolve(__dirname, "dist"),
